d54c80bc83b279d2f424d366b1b786a10bf21a7c
Agattepaille <amelie.gattepaille@hotmail.fr>
2024-12-17 16:33:17 +0100

    ok

--- JavaQuete/Decipherer.java
+++ JavaQuete/Decipherer.java
@@ -4,16 +4,15 @@ class Decipherer {
     public static String decipher(String message) {
         int count = message.length();
         int keyNumber = count / 2;
-        message = message.replace("@#?", " ");
-        String newMessage = message.substring(5, keyNumber);
+        String newMessage = message.substring(5,5 + keyNumber);
+        newMessage = newMessage.replace("@#?", " ");
         String reverseMessage = new StringBuilder(newMessage).reverse().toString();
         return reverseMessage;
     }
 
     public static void main(String[] args) {
-        System.out.print(decipher("0@sn9tirppa@#?ia'jgtvryko1"));
-        System.out.print(decipher("q8e?wsellecif@#?sel@#?setuotpazdsy0*b9+mw@x1vj"));
-        System.out.print(decipher("aopi?sedohtém@#?sedhtmg+p9l!"));
-
+        System.out.println(decipher("0@sn9sirppa@#?ia'jgtvryko1"));
+        System.out.println(decipher("q8e?wsellecif@#?sel@#?setuotpazdsy0*b9+mw@x1vj"));
+        System.out.println(decipher("aopi?sedohtém@#?sedhtmg+p9l!"));
     }
 }
